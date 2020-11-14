8c20417e1f39110458a469a79c803528ce555c7f
Matthew <goostreymatthew@gmail.com>
2020-11-14 11:08:01 +0000

    tidied app.js

--- src/App.js
+++ src/App.js
@@ -3,26 +3,17 @@ import Navbar from './components/Navbar';
 import TodoList from './components/TodoList'
 import TodoContextProvider from './contexts/TodoContext';
 
-import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
-
 function App() {
   //wraps components in Provider
   //Provider passes down reducer functions and state to children
   return (
-    // <BrowserRouter>
-      <div className="App">
-        <TodoContextProvider>
-          {/* <Redirect to='/' /> */}
-          {/* <Switch>
-            <Route exact path='/'> */}
-              <Navbar />
-              <AddTodo />
-              <TodoList />
-            {/* </Route>
-          </Switch> */}
-        </TodoContextProvider>
-      </div>
-    // </BrowserRouter>
+    <div className="App">
+      <TodoContextProvider>
+        <Navbar />
+        <AddTodo />
+        <TodoList />
+      </TodoContextProvider>
+    </div>
   );
 }
 
